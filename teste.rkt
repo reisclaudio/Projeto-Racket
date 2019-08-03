4cad175f9fad3d2f7b98caf0b6ebf85165ee585f
Felipe Alves Barusso <falvesbarusso@gmail.com>
2019-08-03 15:44:23 -0300

    Quadras

--- teste.rkt
+++ teste.rkt
@@ -1,4 +1,4 @@
-#lang racket
+ #lang racket
 
 ;;Abre o arquivo .geo
 (define geo (open-output-file "teste.geo"))
@@ -6,10 +6,14 @@
 (define (gerarId string numero)
   (string-append string (number->string numero)))
 
-(define (gerarQuadras contadorQuadras)
-  (cond [#t (display "q\n" geo)]))
-        ;;[#t (gerarQuadras (+ contadorQuadras 1))]))
-        ;;[#t gerarQuadras (+ x 135) y (+ i 1) (+ contadorQuadras 1)]))
+(define (gerarQuadras x y i contadorQuadras)
+  (cond [(equal? contadorQuadras 25) 
+            (display (string-append "q " (gerarId "quadra" contadorQuadras) " " (number->string x) " " (number->string y) " 120 80\n") geo)]
 
+        [(equal? i 5) 
+            (display (string-append "q " (gerarId "quadra" contadorQuadras) " " (number->string x) " " (number->string y) " 120 80\n") geo) 
+            (gerarQuadras 25 (+ y 95) 0 (+ contadorQuadras 1))]
 
-(close-output-port geo)
+        [#t 
+            (display (string-append "q " (gerarId "quadra" contadorQuadras) " " (number->string x) " " (number->string y) " 120 80\n") geo) 
+            (gerarQuadras (+ x 135) y (+ i 1) (+ contadorQuadras 1))])) 
