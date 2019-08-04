a4cb512754fa0391e6e8650be1222ad1c809c052
Felipe Alves Barusso <falvesbarusso@gmail.com>
2019-08-04 11:30:39 -0300

    Funções matemáticas corrigidas

--- trabalho2.rkt
+++ trabalho2.rkt
@@ -13,28 +13,28 @@
 
 
 
-(define (gerarElementos x y w h i contadorQuadras contadorHidrantes contadorRadiosBase boleanoSemaforo)
+(define (gerarElementos tamanhoCidade x y w h i contadorQuadras contadorHidrantes contadorRadiosBase boleanoSemaforo)
     (cond
         [(equal? boleanoSemaforo 0)
-           (imprimeSemaforos (+ x 127.5) (+ y 87.5) 0 0)
-           (gerarElementos x y w h i contadorQuadras contadorHidrantes contadorRadiosBase 1)]
+           (imprimeSemaforos tamanhoCidade (+ x 127.5) (+ y 87.5) 0 0)
+           (gerarElementos tamanhoCidade x y w h i contadorQuadras contadorHidrantes contadorRadiosBase 1)]
 
-        [(equal? contadorQuadras 24)
+        [(equal? contadorQuadras(- (* tamanhoCidade tamanhoCidade) 1))
            (imprimeQuadras x y w h contadorQuadras)
            (imprimeHidrantes x y contadorHidrantes)
            (imprimeRadiosBase x y contadorRadiosBase)]
 
-        [(equal? i 4)
+        [(equal? i (- tamanhoCidade 1))
            (imprimeQuadras x y w h contadorQuadras)
            (imprimeHidrantes x y contadorHidrantes)
            (cond [(equal? (remainder contadorQuadras 2) 0) (imprimeRadiosBase x y contadorRadiosBase)])
-           (gerarElementos 25 (+ y 95) w h 0 (+ contadorQuadras 1) (+ contadorHidrantes 1) (+ contadorRadiosBase 1) 1)]
+           (gerarElementos tamanhoCidade 25 (+ y 95) w h 0 (+ contadorQuadras 1) (+ contadorHidrantes 1) (+ contadorRadiosBase 1) 1)]
 
         [#t
            (imprimeQuadras x y w h contadorQuadras)
            (imprimeHidrantes x y contadorHidrantes)
            (cond [(equal? (remainder contadorQuadras 2) 0) (+ 1 contadorRadiosBase) (imprimeRadiosBase x y contadorRadiosBase)])
-           (gerarElementos (+ x 135) y w h (+ i 1) (+ contadorQuadras 1) (+ contadorHidrantes 1) (+ contadorRadiosBase 1) 1)]))
+           (gerarElementos tamanhoCidade (+ x 135) y w h (+ i 1) (+ contadorQuadras 1) (+ contadorHidrantes 1) (+ contadorRadiosBase 1) 1)]))
 
 
 
@@ -56,22 +56,22 @@
 
 
 
-(define(imprimeSemaforos x y i contadorSemaforos)
+(define(imprimeSemaforos tamanhoCidade x y i contadorSemaforos)
     (cond
-        [(equal? contadorSemaforos 15)
+        [(equal? contadorSemaforos (* (- tamanhoCidade 1) (- tamanhoCidade 1)))
            (display (string-append "s " (gerarId "semaforo" contadorSemaforos) " " (number->string x) " " (number->string y) "\n") geo)]
 
-        [(equal? i 3)
+        [(equal? i (- tamanhoCidade 2))
            (display (string-append "s " (gerarId "semaforo" contadorSemaforos) " " (number->string x) " " (number->string y) "\n") geo)
-           (imprimeSemaforos 127.5 (+ y 95) 0 (+ contadorSemaforos 1))]
+           (imprimeSemaforos tamanhoCidade 127.5 (+ y 95) 0 (+ contadorSemaforos 1))]
 
         [#t
            (display (string-append "s " (gerarId "semaforo" contadorSemaforos) " " (number->string x) " " (number->string y) "\n") geo)
-           (imprimeSemaforos (+ x 127.5) y (+ i 1) (+ contadorSemaforos 1))]))
+           (imprimeSemaforos tamanhoCidade (+ x 135) y (+ i 1) (+ contadorSemaforos 1))]))
 
 
 ;; Bloco de codigo piloto
-(gerarElementos 25 25 120 80 0 0 0 0 0)
+(gerarElementos 5 25 25 120 80 0 0 0 0 0)
 (close-output-port geo)
 (close-output-port geo2)
 (close-output-port geo3)
