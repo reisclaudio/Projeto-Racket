b9626b1dfd2d0aac4220a56317738dc4e05ea363
Felipe Alves Barusso <falvesbarusso@gmail.com>
2019-08-04 17:01:24 -0300

    Listas de elementos, testes de formas

--- trabalho2.rkt
+++ trabalho2.rkt
@@ -5,6 +5,9 @@
 (define geo2 (open-output-file "b2.geo"))
 (define geo3 (open-output-file "b3.geo"))
 
+(define qry (open-output-file "q1.qry"))
+(define qry2 (open-output-file "q2.qry"))
+(define qry3 (open-output-file "q3.qry"))
 
 
 ;; Recebe uma string e um numero a string de ambos concatenados
@@ -77,31 +80,74 @@
 
 
 
-(define(testesFormas arquivo1 arquivo2 arquivo3)
-    (define x1 (random 660))
-    (define y1 (random 460))
-    (display (string-append "c " "1 " "15 " (number->string x1) " " (number->string y1) "\n") arquivo1)
+(define(testesFormas arquivoG1 arquivoG2 arquivoG3 arquivoQ1 arquivoQ2 arquivoQ3)
+    (define x1 (random 660)) (define y1 (random 460))
+    (display (string-append "c " "1 " "15 " (number->string x1) " " (number->string y1) " midnightblue " "black" "\n") arquivoG1)
+              
+    (define x2 (random 660)) (define y2 (random 460))
+    (display (string-append "r " "2 " "30 " "30 " (number->string x2) " " (number->string y2) " midnightblue " "black" "\n") arquivoG1)
 
-    (define x2 (random 660))
-    (define y2 (random 460))
-    (display (string-append "r " "2 " "30 " "30 " (number->string x2) " " (number->string y2) "\n") arquivo1)
+    ;; Valores de teste 1
+    (define t1 (random 660)) (define t2 (random 460)) (define t3 (random 660)) (define t4 (random 460))
+
+    ;; Testes 1
+    (display "o? 1 2\n" arquivoQ1)
+    (display (string-append "i? 1 " (number->string t1) " " (number->string t2) "\n") arquivoQ1)
+    (display (string-append "i? 2 " (number->string t3) " " (number->string t4) "\n") arquivoQ1)
+    (display "d? 1 2\n" arquivoQ1)
+    (display "bb evandroforte\n" arquivoQ1)
 
     (define x3 (random 1335))
     (define y3 (random 935))
-    (display (string-append "c " "1 " "15 " (number->string x3) " " (number->string y3) "\n") arquivo2)
+    (display (string-append "c " "1 " "15 " (number->string x3) " " (number->string y3) " midnightblue " "black" "\n") arquivoG2)
 
     (define x4 (random 1335))
     (define y4 (random 935))
-    (display (string-append "r " "2 " "30 " "30 " (number->string x4) " " (number->string y4) "\n") arquivo2)
+    (display (string-append "r " "2 " "30 " "30 " (number->string x4) " " (number->string y4) " midnightblue " "black" "\n") arquivoG2)
+
+    ;; Valores de teste 2
+    (define t5 (random 1335)) (define t6 (random 935)) (define t7 (random 1335)) (define t8 (random 935))
+
+    ;; Testes 2
+    (display "o? 1 2\n" arquivoQ2)
+    (display (string-append "i? 1 " (number->string t5) " " (number->string t6) "\n") arquivoQ2)
+    (display (string-append "i? 2 " (number->string t7) " " (number->string t8) "\n") arquivoQ2)
+    (display "d? 1 2\n" arquivoQ2)
+    (display "bb evandroforte\n" arquivoQ2)
 
     (define x5 (random 2010))
     (define y5 (random 1410))
-    (display (string-append "c " "1 " "15 " (number->string x5) " " (number->string y5) "\n") arquivo3)
+    (display (string-append "c " "1 " "15 " (number->string x5) " " (number->string y5) " midnightblue " "black" "\n") arquivoG3)
 
     (define x6 (random 2010))
     (define y6 (random 1410))
-    (display (string-append "r " "2 " "30 " "30 " (number->string x6) " " (number->string y6) "\n") arquivo3)
+    (display (string-append "r " "2 " "30 " "30 " (number->string x6) " " (number->string y6) " midnightblue " "black" "\n") arquivoG3)
+
+    ;; Valores de teste 3
+    (define t9 (random 2010)) (define t10 (random 1410)) (define t11 (random 2010)) (define t12 (random 1410))
+
+    ;; Testes 3
+    (display "o? 1 2\n" arquivoQ3)
+    (display (string-append "i? 1 " (number->string t9) " " (number->string t10) "\n") arquivoQ3)
+    (display (string-append "i? 2 " (number->string t11) " " (number->string t12) "\n") arquivoQ3)
+    (display "d? 1 2\n" arquivoQ3)
+    (display "bb evandroforte\n" arquivoQ3)
 )
+
+
+
+;; Gera lista dos elementos (exceto hidrante)
+(define (gerarLista elemento numId numMax lista)
+  (cond
+    [(equal? numId numMax) lista]
+    [#t (define listaAux (cons (gerarId elemento numId) lista)) (gerarLista elemento (+ numId 1) numMax listaAux)]))
+ 
+;; Gera lista dos hidrantes
+(define (gerarListaHidrante numQuadra numHidrante numMax lista)
+  (cond
+    [(equal? numQuadra numMax) lista]
+    [(equal? numHidrante 4) (define listaAux (cons (string-append (gerarId "hidrante" numQuadra) "." (number->string numHidrante)) lista)) (gerarListaHidrante (+ numQuadra 1) 1 numMax listaAux)]
+    [#t (define listaAux (cons (string-append (gerarId "hidrante" numQuadra) "." (number->string numHidrante)) lista)) (gerarListaHidrante numQuadra (+ numHidrante 1) numMax listaAux)]))
 
 
 
@@ -110,12 +156,30 @@
 (gerarElementos 10 25 25 120 80 0 0 0 0 0 geo2)
 (gerarElementos 15 25 25 120 80 0 0 0 0 0 geo3)
 
-(testesFormas geo geo2 geo3)
+(testesFormas geo geo2 geo3 qry qry2 qry3)
 
+(define listaQuadrasMenor (gerarLista "quadra" 0 25 (list null)))
+(define listaQuadrasMedio (gerarLista "quadra" 0 100 (list null)))
+(define listaQuadrasMaior (gerarLista "quadra" 0 225 (list null)))
+ 
+(define listaSemaforosMenor (gerarLista "semaforo" 0 16 (list null)))
+(define listaSemaforosMedio (gerarLista "semaforo" 0 81 (list null)))
+(define listaSemaforosMaior (gerarLista "semaforo" 0 196 (list null)))
+ 
+(define listaRadioBasesMenor (gerarLista "radioBase" 0 13 (list null)))
+(define listaRadioBasesMedio (gerarLista "radioBase" 0 51 (list null)))
+(define listaRadioBasesMaior (gerarLista "radioBase" 0 113 (list null)))
+ 
+(define listaHidranteMenor (gerarListaHidrante 0 1 25 (list null)))
+(define listaHidranteMedio (gerarListaHidrante 0 1 100 (list null)))
+(define listaHidranteMaior (gerarListaHidrante 0 1 225 (list null)))
 
 (close-output-port geo)
 (close-output-port geo2)
 (close-output-port geo3)
+(close-output-port qry)
+(close-output-port qry2)
+(close-output-port qry3)
 
 
            
